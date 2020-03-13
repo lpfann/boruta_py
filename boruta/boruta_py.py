eb2918c3ec64a669300416a2d0a69a8703c53e9e
Lukas Pfannschmidt <lukas@lpfann.me>
2020-03-13 22:06:14 +0100

    use correct random state seed for lightgbm

--- boruta/boruta_py.py
+++ boruta/boruta_py.py
@@ -301,7 +301,7 @@ class BorutaPy(BaseEstimator, TransformerMixin):
                 self.estimator.set_params(n_estimators=n_tree)
 
             # make sure we start with a new tree in each iteration
-            self.estimator.set_params(random_state=self.random_state.get_state()[2])
+            self.estimator.set_params(random_state=self.random_state.randint(1e10))
 
             # add shadow attributes, shuffle them and train estimator, get imps
             cur_imp = self._add_shadows_get_imps(X, y, dec_reg)
